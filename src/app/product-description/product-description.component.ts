cca5db7cb77f5c7878bf3d8b5bf5bafde8eb6185
Yammani Krishna <ydlkrishna@gmail.com>
2018-05-05 22:31:12 +0530

    added interface

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import '../product.service';
 import { ProductService } from '../product.service';
+import {Album} from '../album';
 
 @Component({
   selector: 'app-product-description',
@@ -9,7 +10,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
