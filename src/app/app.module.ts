310348fbca469e53909cf77eb5b641fd74fd0aec
Yammani Krishna <ydlkrishna@gmail.com>
2018-05-06 10:21:37 +0530

    added product trackinglist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 import {ProductService} from './product.service';
-import { ProductTrackinglistComponent } from './product-trackinglist/product-trackinglist.component';
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTrackinglistComponent
+    ProductTracklistingComponent
   ],
   imports: [
     BrowserModule,
