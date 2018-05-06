e3dac8833d4e1c5591adffff2fec1f0eec081896
Yammani Krishna <ydlkrishna@gmail.com>
2018-05-06 11:09:16 +0530

    added products list

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Album} from './album';
+import {Product} from './product';
 import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json' ;
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -15,4 +17,7 @@ export class ProductService {
     return this._http.get( this._albumUrl).map(respone => <Album>respone.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+  }
 }
